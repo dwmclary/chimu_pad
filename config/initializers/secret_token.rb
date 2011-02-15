b866818569aa5b4180760a49304cede897ff4c33
Daniel McClary <dwmclary@cheme-55.chem-eng.northwestern.edu>
2011-02-15 15:23:01 -0600

    Added initial rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChimuPad::Application.config.secret_token = '7565d880d54d4ab1c36045dd3e26c47e9c435804eaca3e5c50ff9b3770de30a81ad94226d42ebb3c7ea0cad5803f1fb0cf3484abfeb2693c1602209ab43c177d'
